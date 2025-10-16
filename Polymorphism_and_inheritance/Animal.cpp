be490ffda368daccc2849a451399392f57197488
santonet <nik941839@gmail.com>
2025-10-16 16:22:04 +0700

    example class Animal

---
+++ Polymorphism_and_inheritance/Animal.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+
+class Animal {
+private:
+    std::string name;
+
+public:
+    Animal(const std::string& n): name(n) {
+    }
+
+    const std::string& GetName() const {
+        return name;
+    }
+
+    virtual std::string Voice() const = 0;
+
+    virtual ~Animal() {
+    }
+};
+
+class Cat: public Animal {
+public:
+    Cat(const std::string& n): Animal(n) {
+    }
+
+    std::string Voice() const override {
+        return "Meow!";
+    }
+};
+
+class Dog: public Animal {
+public:
+    Dog(const std::string& n): Animal(n) {
+    }
+
+    std::string Voice() const override {
+        return "Woof!";
+    }
+};
+
+void Process(const Animal& creature) {
+    std::cout << creature.GetName() << ": " << creature.Voice() << "\n";
+}
+
+
+int main() {
+
+    // Подробности в параграфе 4.6
+
+    std::vector<std::unique_ptr<Animal>> zoo;
+
+
+    zoo.emplace_back(std::make_unique<Cat>("Tom"));
+
+    zoo.emplace_back(std::make_unique<Dog>("Buffa"));
+
+
+    Process(*zoo[0]);  // Tom: Meow!
+
+    Process(*zoo[1]);  // Buffa: Woof!
+}
