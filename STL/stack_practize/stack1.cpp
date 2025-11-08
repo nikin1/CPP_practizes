e7765e8f13cef73954c2a603dc75e9fb0fcaac3d
santonet <nik941839@gmail.com>
2025-11-08 13:22:57 +0700

    easy stack practize

---
+++ STL/stack_practize/stack1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+    stack<int> st;
+    st.push(10);
+    st.push(5);
+    
+    // Accessing top element
+    cout << "Top element: " << st.top() << endl;
+    
+    // Popping an element
+    st.pop();
+    cout << "Top element after pop: " << st.top() << endl;
+    return 0;
+}
