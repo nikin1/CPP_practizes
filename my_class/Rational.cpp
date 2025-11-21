9777a2ee13782eb1a581aca08001140db608e8f8
santonet <nik941839@gmail.com>
2025-11-21 11:42:45 +0700

    myclass 2.0

--- my_class/Rational.cpp
+++ my_class/Rational.cpp
@@ -1,90 +1,156 @@
 #include <iostream>
+#include <numeric>
+#include <stdexcept>
 
 class Rational {
-
 private:
     int num;
     int denom;
+
+    void normalize() {
+        if (denom == 0) {
+            throw std::invalid_argument("Denominator cannot be zero");
+        }
+        int gcd = std::gcd(num, denom);
+        num /= gcd;
+        denom /= gcd;
+        if (denom < 0) {   // Знаменатель положительный
+            denom = -denom;
+            num = -num;
+        }
+    }
+
 public:
-    Rational(int x, int y) {
-        num = x;
-        denom = y;
-    }
-    Rational(): num(0), denom(-1) {}
-
-
-    int get_num() {
-        return num;
-    }
-    int get_denom() {
-        return denom;
+    Rational(int x = 0, int y = 1) : num(x), denom(y) {
+        normalize();
     }
 
-
-    void set_num(int x) {
-        num = x;
-    }
-    void set_denom(int y) {
-        denom = y;
-    }
-
-
-    // 1-ый const - не меняем число b, второй - не меняем текущий объект
-    Rational operator-(const Rational& b) const {
-        return Rational(num - b.num, b.denom);
-    }
-    Rational operator-(int a) const {
-        return Rational(num - a * denom, denom);
-    }
-    Rational operator-() const {
-        return Rational(num * -1, denom);
-    }
-
-
-
-
+    int Numerator() const { return num; }
+    int Denominator() const { return denom; }
 
     Rational operator+(const Rational& b) const {
-        return Rational(num + b.num, b.denom);
-    }
-    Rational operator+(int a) const {
-        return Rational(num + a * denom, denom);
-    }
-    Rational operator+() const {
-        return Rational(num, denom);
+        return Rational(num * b.denom + b.num * denom, denom * b.denom);
     }
 
+    Rational operator-(const Rational& b) const {
+        return Rational(num * b.denom - b.num * denom, denom * b.denom);
+    }
 
     Rational operator*(const Rational& b) const {
         return Rational(num * b.num, denom * b.denom);
     }
-    Rational operator*(int a) const {
-        return Rational(num * a, denom);
-    }
 
     Rational operator/(const Rational& b) const {
+        if (b.num == 0) {
+            throw std::invalid_argument("Division by zero");
+        }
         return Rational(num * b.denom, denom * b.num);
     }
-    Rational operator/(int a) const {
-        return Rational(num, denom * a);
+
+    Rational operator+(int a) const {
+        return *this + Rational(a);
     }
 
+    Rational operator-(int a) const {
+        return *this - Rational(a);
+    }
 
+    Rational operator*(int a) const {
+        return *this * Rational(a);
+    }
+
+    Rational operator/(int a) const {
+        if (a == 0) {
+            throw std::invalid_argument("Division by zero");
+        }
+        return *this / Rational(a);
+    }
+
+    Rational& operator+=(const Rational& b) {
+        *this = *this + b;
+        return *this;
+    }
+
+    Rational& operator-=(const Rational& b) {
+        *this = *this - b;
+        return *this;
+    }
+
+    Rational& operator*=(const Rational& b) {
+        *this = *this * b;
+        return *this;
+    }
+
+    Rational& operator/=(const Rational& b) {
+        *this = *this / b;
+        return *this;
+    }
+
+    Rational& operator+=(int a) {
+        *this = *this + a;
+        return *this;
+    }
+
+    Rational& operator-=(int a) {
+        *this = *this - a;
+        return *this;
+    }
+
+    Rational& operator*=(int a) {
+        *this = *this * a;
+        return *this;
+    }
+
+    Rational& operator/=(int a) {
+        if (a == 0) {
+            throw std::invalid_argument("Division by zero");
+        }
+        *this = *this / a;
+        return *this;
+    }
+
+    bool operator==(const Rational& b) const {
+        return num == b.num && denom == b.denom;
+    }
+
+    bool operator!=(const Rational& b) const {
+        return !(*this == b);
+    }
+
+    Rational operator-() const {
+        return Rational(-num, denom);
+    }
+
+    Rational operator+() const {
+        return *this;
+    }
 };
 
+// Оператор умножения int * Rational вне класса
+Rational operator*(int a, const Rational& r) {
+    return r * a;
+}
+Rational operator/(int a, const Rational& r) {
+    return r / a;
+}
 
+
+
+// Можно добавить другие операторы с int слева по аналогии
 
 int main() {
     Rational a(1, 2);
-    Rational b(3, 2);
-    // y = a + b;
-    // Rational y = b - a;
+    Rational b(3, 4);
 
-    Rational y = a + 1;
+    Rational c = a + b;
+    Rational d = c - 1;
+    Rational e = d * 2;
+    Rational f = 2 * e;  // Работает благодаря свободному operator*
 
-    std::cout << "y = " << y.get_num() << "/" << y.get_denom() << std::endl;
- 
+    std::cout << "c = " << c.Numerator() << "/" << c.Denominator() << std::endl;
+    std::cout << "d = " << d.Numerator() << "/" << d.Denominator() << std::endl;
+    std::cout << "e = " << e.Numerator() << "/" << e.Denominator() << std::endl;
+    std::cout << "f = " << f.Numerator() << "/" << f.Denominator() << std::endl;
 
     return 0;
 }
-
